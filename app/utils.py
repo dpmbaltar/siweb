6f3548434ed6dd7ad6153ae29a3be26ab94aa0fe
Diego Pablo Matías Baltar <dpmbaltar@gmail.com>
2024-09-29 20:47:41 -0300

    Agregar decorador login_required

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,20 @@
-import os
-from werkzeug.utils import secure_filename
+from functools import wraps
+from flask import make_response, jsonify, redirect, url_for, session
 
 
 def allowed_file(filename, allowed_extensions):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
+
+
+def logged_in():
+    return 'user' in session
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not logged_in():
+            # return redirect(url_for('autenticacion_login', next=request.url))
+            return make_response(jsonify({'error': 'No ha iniciado sesión'}), 401)
+        return f(*args, **kwargs)
+    return decorated_function
