03e66f9a4b3300b677b08f0d003950c1500ffc15
Diego Pablo Matías Baltar <dpmbaltar@gmail.com>
2024-09-28 22:19:23 -0300

    Agregar configuración para archivos subidos

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_seeder import FlaskSeeder
@@ -8,7 +9,7 @@ from .config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
-api = Api(title="SIWEB API", 
+api = Api(title="SIWEB API",
           version="1.0",
           description="Documentación de mi API",
           doc="/docs"
@@ -20,6 +21,10 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    # Crear carpeta para archivos si no existe
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+
     db.init_app(app)
     migrate.init_app(app, db)
     seeder.init_app(app, db)
@@ -28,7 +33,9 @@ def create_app():
 
     from .routes.usuario import api as usuario_namespace
     from .routes.publicacion import api as publicacion_namespace
+    from .routes.archivo import api as archivo_namespace
     api.add_namespace(usuario_namespace)
     api.add_namespace(publicacion_namespace)
+    api.add_namespace(archivo_namespace)
 
     return app
--- app/config.py
+++ app/config.py
@@ -5,3 +5,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL',
                                         'mysql://user:pass@localhost/dbname')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # Tamaño máximo de archivo (16MB)
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
