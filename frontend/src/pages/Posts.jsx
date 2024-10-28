31b0b7c730e91a85aa1923d06d46de1360346ccf
Nicolás Comeron <nicolas.comeron@est.fi.uncoma.edu.ar>
2024-10-27 21:46:59 -0300

    Actualizar página Posts

--- frontend/src/pages/Posts.jsx
+++ frontend/src/pages/Posts.jsx
@@ -1,7 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import PostCard from '../components/postCard/PostCard';
+
 export default function Posts() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    axios
+      .get('http://127.0.0.1:5000/publicaciones/')
+      .then((response) => {
+        setPosts(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <>
+    <div className='open-sans-text'>
       <h1 className='open-sans-title'>Posts</h1>
-    </>
+      {posts.map((item) => (
+        <PostCard
+          key={item.id}
+          usuario={item.usuario}
+          fechaCreado={item.fecha_creado}
+          titulo={item.titulo}
+          descripcion={item.contenido}
+        />
+      ))}
+    </div>
   );
 }
