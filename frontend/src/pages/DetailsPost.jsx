61c797016b67ae47a74c0251d5188d7ac1313f54
Diego Pablo Matías Baltar <dpmbaltar@gmail.com>
2024-10-31 23:29:04 -0300

    Agregar datos en los detalles de publicación

--- frontend/src/pages/DetailsPost.jsx
+++ frontend/src/pages/DetailsPost.jsx
@@ -19,7 +19,7 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 const Img = styled('img')({
-  width: 330,
+  width: '100%',
   height: '100%',
   flex: 1,
   objectFit: 'fill',
@@ -28,44 +28,48 @@ const Img = styled('img')({
 
 export default function DetailsPost() {
   let { state } = useLocation();
+  const post = state.datosPublicacion;
+  const { usuario, mascotas, archivos } = post;
 
   return (
     <>
-      <h2 className='open-sans-title'>Detalles del post</h2>
+      <h2 className='open-sans-title'>Detalles de la publicación</h2>
       <Item sx={{ mx: 'auto', p: 2 }}>
         <Stack direction='row' spacing={2} sx={{ alignItems: 'center', pb: 2 }}>
-          <UserAvatar usuario={state.datosPublicacion.usuario} />
+          <UserAvatar usuario={usuario} />
           <Stack spacing={2} sx={{ alignItems: 'center' }}>
             <Typography>
-              {state.datosPublicacion.usuario.nombre}{' '}
-              {state.datosPublicacion.usuario.apellido}
+              {usuario.nombre}{' '}
+              {usuario.apellido}
             </Typography>
           </Stack>
         </Stack>
-        <Img src='https://via.placeholder.com/300' alt='mascota' />
+        <Img src={`http://localhost:5000/archivos/${archivos[0].id}`} alt='mascota' />
         <Typography component='div' variant='body1' textAlign={'left'}>
-          <Typography variant='h6'>Datos</Typography>
+          <Typography variant='h6'>Detalles</Typography>
           <Divider />
-          <p><b>Descripción:</b> {state.datosPublicacion.contenido}</p>
-          <p>
-            <b>Contacto: </b>
+          <div>
+            <p>{post.contenido}</p>
+            <h4>Contacto</h4>
             <Chip
-              label={state.datosPublicacion.tel_contacto}
+              label={post.tel_contacto}
               variant="outlined"
               component="a"
-              href={`tel:${state.datosPublicacion.tel_contacto}`}
+              href={`tel:${post.tel_contacto}`}
               clickable
               />
-          </p>
-          <p><b>Nombre:</b> </p>
-          <p><b>Edad:</b> </p>
-          <p><b>Área:</b> </p>
+            <h4>Mascotas</h4>
+            {mascotas.map((mascota) => (
+              <Chip key={mascota.id} label={mascota.nombre} />
+            ))}
+            <h4>Lugar aproximado</h4>
+          </div>
         </Typography>
         <Divider />
 
         <CustomMap
-          lat={state.datosPublicacion.area_lat}
-          lng={state.datosPublicacion.area_lng}
+          lat={post.area_lat}
+          lng={post.area_lng}
           />
       </Item>
     </>
